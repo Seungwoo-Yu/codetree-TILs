059f7dc00ba66e1e4c74e2e47d1a1da09f7ca8d9
Seungwoo-Yu <51766845+Seungwoo-Yu@users.noreply.github.com>
2024-01-30 00:06:39 +0900

    Title: 거품 정렬 구현, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240130/거품 정렬 구현/implement-bubble-sort.py
@@ -0,0 +1,15 @@
+n = int(input())
+nums = list(map(int, input().split(' ')))
+sort = True
+
+while sort:
+    sort = False
+
+    for j in range(len(nums) - 1):
+        if nums[j] > nums[j + 1]:
+            sort = True
+            tmp = nums[j]
+            nums[j] = nums[j + 1]
+            nums[j + 1] = tmp
+
+print(' '.join(map(str, nums)))
