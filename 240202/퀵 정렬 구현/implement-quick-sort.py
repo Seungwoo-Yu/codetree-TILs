caa34936f951d247f97fce1b7a7e2fadc70c46f9
Seungwoo-Yu <51766845+Seungwoo-Yu@users.noreply.github.com>
2024-02-02 02:17:01 +0900

    Title: 퀵 정렬 구현, Time: 372ms, Memory: 31MB, Status: Passed - CodeTree

--- 240202/퀵 정렬 구현/implement-quick-sort.py
+++ 240202/퀵 정렬 구현/implement-quick-sort.py
@@ -18,7 +18,7 @@ def pivot(_nums, _start, _end):
 
 def quick_sort(_nums, _start, _end):
     if _start < _end:
-        if len(_nums) >= 3:
+        if _end - _start >= 3:
             pos = pivot(_nums, _start, _end)
             if pos < _end:
                 tmp = _nums[_end]
@@ -26,36 +26,26 @@ def quick_sort(_nums, _start, _end):
                 _nums[pos] = tmp
 
         pos = sort(_nums, _start, _end)
-        if pos <= _end:
-            quick_sort(_nums, _start, pos - 1)
-            quick_sort(_nums, pos + 1, _end)
+        quick_sort(_nums, _start, pos - 1)
+        quick_sort(_nums, pos + 1, _end)
 
 
 def sort(_nums, _start, _end):
     pvt = _nums[_end]
-    i = 0
-    j = 0
+    i = _start - 1
 
-    while j < len(_nums):
-        if _nums[i] < pvt:
+    for j in range(_start, _end):
+        if _nums[j] < pvt:
             i += 1
-            j += 1
-
-        if _nums[j] > pvt:
-            j += 1
-        elif _nums[i] >= pvt:
             tmp = _nums[i]
             _nums[i] = _nums[j]
             _nums[j] = tmp
-            i += 1
-            j += 1
 
-    if j < len(_nums):
-        tmp = _nums[i]
-        _nums[i] = _nums[_end]
-        _nums[_end] = tmp
+    tmp = _nums[i + 1]
+    _nums[i + 1] = _nums[_end]
+    _nums[_end] = tmp
 
-    return i
+    return i + 1
 
 
 quick_sort(nums, 0, len(nums) - 1)
